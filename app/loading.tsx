6795676ff183d7ea29fa5997818f0a7ffa3d90e7
Thomas Schorn <thomas@schorn.io>
2023-07-25 16:44:37 +0200

    [minor] loading screen

---
+++ app/loading.tsx
@@ -0,0 +1,14 @@
+export default function Loading() {
+  return (
+    <div
+      style={{
+        alignItems: 'center',
+        display: 'flex',
+        height: '100vh',
+        justifyContent: 'center',
+      }}
+    >
+      <div>...</div>
+    </div>
+  );
+}
