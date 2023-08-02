97a97c178332e64c9a24287b6e7e90edaf9fb1a6
Thomas Schorn <t@schorn.io>
2023-08-02 17:08:23 +0200

    [patch] vercel error

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        // FROM https://github.com/vercel/next.js/issues/42641#issuecomment-1615901228
+        './node_modules/@swc/core-linux-x64-gnu',
+        './node_modules/@swc/core-linux-x64-musl',
+      ],
+    },
+  },
   images: {
     domains: ['fifty-runs-files.s3.fr-par.scw.cloud'],
   },
