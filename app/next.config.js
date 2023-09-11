1cd116f946ec1397d76efbe199a8a862c6b0dfed
Thomas Schorn <t@schorn.io>
2023-09-11 19:02:24 +0200

    [minor] disable running and donating for now

--- app/next.config.js
+++ app/next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     domains: [
       'fifty-runs-files.s3.fr-par.scw.cloud',
       'ccqov2xqivigvtrz.public.blob.vercel-storage.com',
+      'wt2duksjvdyzu9rt.public.blob.vercel-storage.com',
     ],
     minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
   },
