2bac6c345ef6e929a1081ba2e7d355720b93b94c
Thomas Schorn <t@schorn.io>
2023-09-11 18:39:02 +0200

    [minor] customizable main header

--- app/components/view/Header/Header.tsx
+++ app/components/view/Header/Header.tsx
@@ -4,12 +4,25 @@ import Link from 'next/link';
 import { UserLabel } from '@/components/composed/UserLabel';
 import { getCurrentSession } from '@/util/server/getCurrentSession';
 import { prisma } from '@/prisma';
+import { getMainHeader } from '@/service/getMainHeader';
+import { log } from 'console';
 
 export async function Header() {
   const session = await getCurrentSession();
-  const user = await (session
-    ? prisma.user.findUnique({ where: { id: session.userId } })
-    : undefined);
+  const [user, mainHeader] = await Promise.all([
+    session
+      ? prisma.user.findUnique({ where: { id: session.userId } })
+      : undefined,
+    getMainHeader(),
+  ]);
+  console.log(mainHeader?.attributes.links, 'mainHeader');
+  const loggedIn = Boolean(user);
+  const linkVisible = loggedIn ? 'loggedIn' : 'loggedOut';
+
+  const mainLogo = mainHeader?.attributes.logo?.data?.attributes;
+  const mainLinks = mainHeader?.attributes.links.filter(
+    ({ visible }) => visible === 'both' || visible === linkVisible,
+  );
 
   return (
     <header className="sticky top-0 z-50 bg-white">
@@ -17,47 +30,35 @@ export async function Header() {
         <div className="mx-auto w-full max-w-screen-xl p-5">
           <div className="flex items-center justify-between">
             <Link href="/">
-              <Image
-                alt="50runs"
-                height={50}
-                src="/image/Logo_50runs_neu.png"
-                width={155}
-                priority={true}
-              />
+              {mainLogo ? (
+                <Image
+                  alt="50runs"
+                  height={50}
+                  src={mainLogo.url}
+                  width={155}
+                  priority={true}
+                />
+              ) : undefined}
             </Link>
             <HeaderMenu>
               <div className="bg-white max-lg:border-y max-lg:border-y-neutral-300 max-lg:p-10 max-lg:shadow-lg">
                 <div className="flex items-center gap-5 max-lg:flex-col max-lg:gap-10">
-                  <Link className="text-lg font-bold" href="/leaderboard">
-                    Leaderboard
-                  </Link>
-                  <Link className="text-lg font-bold" href="/postings">
-                    Beiträge
-                  </Link>
-                  {user?.name ? (
-                    <>
+                  {mainLinks?.map((link) =>
+                    link.label === '<UserLabel>' && user ? (
                       <UserLabel
                         userImage={user.image}
                         userName={user.name}
                         userNameId={user.nameId}
                       />
+                    ) : (
                       <Link
                         className="text-lg font-bold"
-                        href="/user/logout"
-                        prefetch={false}
+                        href={link.url}
+                        key={link.id}
                       >
-                        Abmelden
+                        {link.label}
                       </Link>
-                    </>
-                  ) : (
-                    <>
-                      <Link className="text-lg font-bold" href="/user/login">
-                        Anmelden
-                      </Link>
-                      <Link className="text-lg font-bold" href="/user/register">
-                        Registrieren
-                      </Link>
-                    </>
+                    ),
                   )}
                 </div>
               </div>
---
+++ app/service/getMainHeader.ts
@@ -0,0 +1,18 @@
+import { MainHeader } from '@/types/MainHeader';
+import { StrapiFindOneResult } from '@schornio/strapi-util/types/StrapiFindOneResult';
+import { fetchStrapi } from '@schornio/strapi-util/fetchStrapi';
+
+export async function getMainHeader() {
+  const result = await fetchStrapi<StrapiFindOneResult<MainHeader>>(
+    '/main-header',
+    {
+      next: {
+        revalidate: 0, //60 * 60 * 24, // 24 hours
+      },
+      query: {
+        populate: ['links', 'logo'],
+      },
+    },
+  );
+  return result?.data;
+}
