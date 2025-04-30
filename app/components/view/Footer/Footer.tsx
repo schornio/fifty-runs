73b7226b44c93665bf55b91e6c2c1c991f5ea982
Thomas Schorn <t@schorn.io>
2025-04-30 08:46:51 +0200

    feature: cyberhouse logo - thank you for your contribution!

--- app/components/view/Footer/Footer.tsx
+++ app/components/view/Footer/Footer.tsx
@@ -5,14 +5,24 @@ export function Footer() {
   return (
     <footer className="flex justify-center bg-atlantis-500">
       <div className="flex w-full max-w-screen-xl items-center justify-between p-5">
-        <Link href="https://www.schorn.io">
-          <Image
-            alt="50runs"
-            height={77}
-            src="/image/schornio.png"
-            width={200}
-          />
-        </Link>
+        <div className="flex items-center">
+          <Link href="https://www.schorn.io">
+            <Image
+              alt="50runs"
+              height={77}
+              src="/image/schornio.png"
+              width={200}
+            />
+          </Link>
+          <Link href="https://www.cyberhouse.at/">
+            <Image
+              alt="Cyberhouse"
+              height={77}
+              src="/image/Cyberhouse_Logo_rgb_200.svg"
+              width={200}
+            />
+          </Link>
+        </div>
         <div className="flex gap-5">
           <Link
             className="font-semibold text-congress-blue-900"
